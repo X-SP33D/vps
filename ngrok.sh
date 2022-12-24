84973b20e27132f4717bfcfd09b9b2024c6af7de
X-SPEED <52790211+X-SP33D@users.noreply.github.com>
2022-12-24 11:06:30 +0100

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -8,5 +8,5 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget -O ngrok.zip https://cdn.jsdelivr.net/gh/X-SP33D/vps@main/ngrok-stable-linux-amd64.zip?raw=true > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
